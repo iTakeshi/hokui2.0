cf98db2710924aa19d5573796ffc840ea6985e88
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-08 15:47:22 +0900

    refs #49 user_statusが0でない場合ははじくこと。user_status = 3のときの処理を考えること。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class ApplicationController < ActionController::Base
   before_filter :authorize_as_user
   protect_from_forgery
@@ -14,9 +16,33 @@ private
     end
   end
 
+  def classify_user_status(user)
+    if user.user_status == 1
+      flash[:error] = 'ELMSメールアドレスの確認が完了していません。ELMS受信BOXを確認してください。'
+    elsif user.user_status == 2
+      flash[:error] = '新規登録申請が承認されるのを待っています。承認されると、ELMSメール宛に通知されます。'
+    elsif user.user_status == 3
+      if user.user_secret_token_expiration_time < Time.now
+        user.user_status = 0
+        user.user_secret_token = nil
+        user.user_secret_token_expiration_time = nil
+        user.save
+        flash[:error] = 'パスワード再設定手続きの途中でしたが、URLの有効期限切れでキャンセルされました。再度お試しください。'
+      else
+        flash[:error] = 'パスワード再設定手続きの途中です。ELMSメールアドレス宛に送信されたメールを確認してください。'
+      end
+    end
+  end
+
   def current_user
     if session[:user_auth_token]
-      User.find_by_user_auth_token(session[:user_auth_token])
+      user = User.find_by_user_auth_token(session[:user_auth_token])
+      if user.user_status == 0
+        user
+      else
+        classify_user_status(user)
+        nil
+      end
     else
       nil
     end
