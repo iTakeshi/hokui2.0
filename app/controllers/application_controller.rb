c4ff3e7d697cfe52d6f24211d1557ec92ec2ae8b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-08 09:55:36 +0900

    refs #31 セッション管理 - authorize_as_user書いた。リダイレクトループで何も見れない。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :authorize_as_user
   protect_from_forgery
+
+private
+  def authorize_as_user
+    redirect_to '/login' unless current_user
+  end
+
+  def current_user
+    if session[:user_auth_token]
+      User.find_by_user_auth_token(session[:user_auth_token])
+    else
+      nil
+    end
+  end
 end
