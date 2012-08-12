224dbcc5feb1fb56f2f594e989a9072b1b63d715
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-12 14:01:41 +0900

    使ってないspec消した

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,18 +1,4 @@
 require "spec_helper"
 
 describe AdminMailer do
-  describe "notify_signup_request" do
-    let(:mail) { AdminMailer.notify_signup_request }
-
-    it "renders the headers" do
-      mail.subject.should eq("Notify signup request")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      mail.body.encoded.should match("Hi")
-    end
-  end
-
 end
