e88a048f33446a1f6ab1ec76a1b35b84ee769d6a
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-12 14:39:10 +0900

    refs #59 アップロードされたHTMLテキストをスクレイピングし、必要情報を登録。 - Subject#get_subject_informations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,3 +1,13 @@
+# coding: utf-8
+
 class Subject < ActiveRecord::Base
   attr_accessible :subject_identifier, :subject_name, :subject_staff, :subject_syllabus_html, :term_identifier
+
+  def get_subject_informations
+    html = Nokogiri::HTML(self.subject_syllabus_html)
+    self.subject_identifier = html.css("#Detail_lbl_sbj_name_e").children[0].text.gsub(/(\)|\[|\])/, '').sub('(', '_').downcase.strip
+    self.subject_name = html.css("#Detail_lbl_sbj_name").children[0].text.strip
+    self.subject_staff = html.css("#Detail_lbl_admin_staff_alias").children[0].text.split('[')[0].sub('　', ' ').strip
+    self.subject_lct_cd = html.css("#Detail_lbl_lct_cd").children[0].text.strip
+  end
 end
