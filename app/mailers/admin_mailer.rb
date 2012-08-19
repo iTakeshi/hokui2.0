9f63a6d6d995b9ebe0d50e1fa1ee0b6dcd913c88
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-19 12:19:45 +0900

    server_nameの扱い変えた

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,10 +2,11 @@
 
 class AdminMailer < ActionMailer::Base
   default from: "hokui93.net@gmail.com"
+  require '/var/app/setting/hokui/server_name.rb'
 
   def notify_signup_request(admin, user)
+    @server_name = hokui_server_name
     @user = user
-
     mail to: admin.user_email, subject: '北医ネット：新規ユーザー承認リクエスト'
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,23 +2,28 @@
 
 class UserMailer < ActionMailer::Base
   default from: "hokui93.net@gmail.com"
+  require '/var/app/setting/hokui/server_name.rb'
 
   def confirm_signup(user)
+    @server_name = hokui_server_name
     @user = user
     mail to: @user.user_email, subject: '北医ネット：新規登録を受け付けました'
   end
 
   def notify_approved(user)
+    @server_name = hokui_server_name
     @user = user
     mail to: @user.user_email, subject: '北医ネット：登録申請が承認されました'
   end
 
   def notify_rejected(user)
+    @server_name = hokui_server_name
     @user = user
     mail to: @user.user_email, subject: '北医ネット：登録申請が拒否されました'
   end
 
   def notify_resetting_password(user)
+    @server_name = hokui_server_name
     @user = user
     mail to: @user.user_email, subject: '北医ネット：パスワードを再設定します'
   end
