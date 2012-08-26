93e2dbd30e613fd3cea9f8bd14853a789d722345
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-26 13:03:23 +0900

    refs #91 Modelを定義すること。バリデーション書くこと。 - validation

--- app/models/freeml_entry.rb
+++ app/models/freeml_entry.rb
@@ -1,6 +1,25 @@
 class FreemlEntry < ActiveRecord::Base
   attr_accessible :freeml_body, :freeml_datetime, :freeml_id, :freeml_title, :freeml_user, :freeml_readable
 
+  validates :freeml_id,
+    presence: true,
+    uniqueness: true
+
+  validates :freeml_user,
+    presence: true
+
+  validates :freeml_body,
+    presence: true
+
+  validates :freeml_title,
+    presence: true
+
+  validates :freeml_datetime,
+    presence: true
+
+  validates :freeml_readable,
+    inclusion: { in: [true, false] }
+
   def self.find(id)
     self.find_by_freeml_id(id)
   end
--- spec/models/freeml_entry_spec.rb
+++ spec/models/freeml_entry_spec.rb
@@ -1,5 +1,85 @@
 require 'spec_helper'
 
 describe FreemlEntry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation:' do
+
+    before(:all) do
+      entry = FreemlEntry.new(
+        freeml_id: 100,
+        freeml_user: 'hoge',
+        freeml_body: 'hello',
+        freeml_title: 'hello',
+        freeml_datetime: Time.now,
+        freeml_readable: true
+      )
+      entry.save!
+    end
+
+    before(:each) do
+      @entry = FreemlEntry.new(
+        freeml_id: 101,
+        freeml_user: 'fuga',
+        freeml_body: 'bye',
+        freeml_title: 'bye',
+        freeml_datetime: Time.now,
+        freeml_readable: false
+      )
+    end
+
+    context "with valid information" do
+      it "should be valid" do
+        @entry.should be_valid
+      end
+    end
+
+    context "without :freeml_id" do
+      it "should not be valid" do
+        @entry.freeml_id = nil
+        @entry.should_not be_valid
+      end
+    end
+
+    context ":freeml_id is not unique" do
+      it "should not be valid" do
+        @entry.freeml_id = 100
+        @entry.should_not be_valid
+      end
+    end
+
+    context "without :freeml_user" do
+      it "should not be valid" do
+        @entry.freeml_user = nil
+        @entry.should_not be_valid
+      end
+    end
+
+    context "without :freeml_body" do
+      it "should not be valid" do
+        @entry.freeml_body = nil
+        @entry.should_not be_valid
+      end
+    end
+
+    context "without :freeml_title" do
+      it "should not be valid" do
+        @entry.freeml_title = nil
+        @entry.should_not be_valid
+      end
+    end
+
+    context "without :freeml_datetime" do
+      it "should not be valid" do
+        @entry.freeml_datetime = nil
+        @entry.should_not be_valid
+      end
+    end
+
+    context "without :freeml_readable" do
+      it "should not be valid" do
+        @entry.freeml_readable = nil
+        @entry.should_not be_valid
+      end
+    end
+
+  end
 end
