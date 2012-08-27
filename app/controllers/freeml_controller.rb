5c7c34a3c895d70f1326818efecf3828da29a563
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-08-27 11:10:39 +0900

    mechanize-ssl問題最終解決

--- app/controllers/freeml_controller.rb
+++ app/controllers/freeml_controller.rb
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+require 'openssl'
+
 class FreemlController < ApplicationController
   skip_before_filter :authorize_as_user, only: :batch
 
@@ -35,7 +37,6 @@ class FreemlController < ApplicationController
     agent = Mechanize.new
     agent.user_agent_alias = 'Mac Safari'
     require '/var/app/setting/hokui/freeml.rb'
-    agent.agent.http.ca_file = '/etc/ssl/certs/ca-certificates.crt'
     agent.post('https://www.freeml.com/ep.umzx/grid/General/node/SpLoginProcess', freeml_login_info)
     doc = Nokogiri::HTML(agent.get('http://www.freeml.com/hokudaimedicine93/topics').body.toutf8)
     doc.css('.entry_r').each do |entry|
